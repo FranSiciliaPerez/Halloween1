273e747e8b86502f0f45e34bd30e921110cbdab5
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2020-11-05 11:05:31 +0100

    Add files via upload

---
+++ javascripts/halloween.js
@@ -0,0 +1,11 @@
+/*FUNCIONES DE PRESENTACION DE HALLOWEEN CURSO 2020/21*/
+function mostrar(identificador){
+    document.getElementById(identificador).style.visibility="visible";
+}
+function ocultar(identificador){
+    document.getElementById(identificador).style.visibility="hidden";
+}
+function sonar(sonido){
+document.getElementById(sonido).play();}
+function parar(sonido){
+document.getElementById(sonido).pause();}
